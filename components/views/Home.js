99dc55186c0eaa3b2c7332928f4738d75226834d
Casey Selby <caseyjselby@gmail.com>
2022-05-25 19:19:29 -0700

    activity

--- components/views/Home.js
+++ components/views/Home.js
@@ -1,8 +1,12 @@
 import html from "html-literal";
 
-export default () => html`
-<section id="jumbotron">
+export default state => html`
+  <section id="jumbotron">
     <h2>SavvyCoders JavaScript Fullstack Bootcamp</h2>
     <a href="index.html">"Call to Action" "Button"</a>
   </section>
+  <h3>
+    Weather in ${state.weather.city} ${state.weather.temp}F, feels like
+    ${state.weather.feelsLike}F
+  </h3>
 `;
